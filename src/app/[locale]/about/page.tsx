d657f913636efcf2dfca4744ee7e314d58f15c3d
leejay <buttercookie210@gmail.com>
2025-08-14 00:53:37 +0900

    feat: add experience and education to work

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
+import ButtonText from '@/components/ui/ButtonText'
 
 export default function About() {
   const t = useTranslations('about')
@@ -9,7 +10,55 @@ export default function About() {
         <Image src={'/icons/apple.png'} width={100} height={100} alt={'apple'} className="my-4" />
         <h1 className="text-6xl font-bold mb-12">About</h1>
         <div>
-          <div>{t('intro')}</div>
+          <section>
+            <div className="break-all">{t('intro')}</div>
+          </section>
+
+          <section>
+            <ButtonText>Experience</ButtonText>
+            <div className="flex flex-col md:flex-row py-4 gap-10 mt-5">
+              <div className="pt-2 shrink-0">
+                <Image
+                  src="/images/about/ponylink_logo.png"
+                  alt="company_logo"
+                  width={160}
+                  height={20}
+                />
+              </div>
+              <div>
+                <div className="text-2xl font-semibold">{t('experience.ponylink.title')}</div>
+                <div>{t('experience.ponylink.subtitle')}</div>
+                <div className="mt-5 whitespace-pre-line">{t('experience.ponylink.desc')}</div>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <ButtonText>Education</ButtonText>
+            {/* UoB */}
+            <div className="flex flex-col md:flex-row py-4 gap-10 mt-5">
+              <div className="w-[160px] flex justify-center">
+                <Image src="/images/about/uob_logo.png" alt="uob_logo" width={100} height={100} />
+              </div>
+              <div>
+                <div className="text-2xl font-semibold">{t('education.uob.title')}</div>
+                <div>{t('education.uob.major')}</div>
+                <div>{t('education.uob.date')}</div>
+              </div>
+            </div>
+
+            {/* SNUE */}
+            <div className="flex flex-col md:flex-row py-4 gap-10 mt-5">
+              <div className="w-[160px] flex justify-center">
+                <Image src="/images/about/snue_logo.png" alt="snue_logo" width={100} height={100} />
+              </div>
+              <div>
+                <div className="text-2xl font-semibold">{t('education.snue.title')}</div>
+                <div>{t('education.snue.major')}</div>
+                <div>{t('education.snue.date')}</div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </>
--- src/components/ui/ButtonText.tsx
+++ src/components/ui/ButtonText.tsx
@@ -1,10 +1,14 @@
 interface ButtonTextProps {
   children: React.ReactNode
-  bgColorStyle: string
-  textColorStyle: string
+  bgColorStyle?: string
+  textColorStyle?: string
 }
 
-export default function ButtonText({ children, bgColorStyle, textColorStyle }: ButtonTextProps) {
+export default function ButtonText({
+  children,
+  bgColorStyle = 'bg-neutral-700',
+  textColorStyle = 'text-white',
+}: ButtonTextProps) {
   return (
     <div
       className={`${bgColorStyle} ${textColorStyle} my-2 mt-10 font-medium border-black px-3 py-1 rounded-full inline-block`}
