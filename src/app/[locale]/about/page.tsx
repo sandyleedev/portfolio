56bc6d08115daf78d0fab573b9ac08f10751ff99
leejay <buttercookie210@gmail.com>
2025-08-13 23:55:02 +0900

    chore: add intro to about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
+import { useTranslations } from 'next-intl'
 
 export default function About() {
+  const t = useTranslations('AboutPage')
   return (
     <>
       <div className="px-6 py-12 max-w-6xl mx-auto">
         <Image src={'/icons/apple.png'} width={100} height={100} alt={'apple'} className="my-4" />
         <h1 className="text-6xl font-bold mb-12">About</h1>
+        <div>
+          <div>{t('intro')}</div>
+        </div>
       </div>
     </>
   )
