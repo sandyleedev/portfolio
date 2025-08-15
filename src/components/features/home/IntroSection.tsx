fd1a4b5159a87ded2bd84e629c7d6ee7c6d85be8
leejay <buttercookie210@gmail.com>
2025-08-15 14:54:58 +0900

    feat: hide vertical scroll bar / apply float animation

--- src/components/features/home/IntroSection.tsx
+++ src/components/features/home/IntroSection.tsx
@@ -7,7 +7,7 @@ export default function IntroSection() {
 
   return (
     <>
-      <div className="flex-none h-screen px-2">
+      <div className="flex-none h-screen px-2 relative">
         <div className={`${mounted ? 'animate-fadeInUp' : ''} opacity-0 text-[20vh]/[20vh]`}>
           Jenna Lee, <br />
           Full Stack Developer <br />2 years Experience
@@ -15,6 +15,7 @@ export default function IntroSection() {
             <Starburst />
           </div>
         </div>
+        <img className="absolute bottom-0 right-0 -z-1 opacity-70 animate-float" src="/images/common/computer.png" />
       </div>
       <div className="h-screen px-2">
         <div className="w-[100vw] text-wrap pt-[20vh] pl-[1vw] text-[20vh]/[20vh]">
