76221e5fd05f2f64afb7c1434be558984c523404
leejay <buttercookie210@gmail.com>
2025-08-23 13:10:06 +0900

    feat: rebuild responsive about section

--- src/components/features/home/AboutSection.tsx
+++ src/components/features/home/AboutSection.tsx
@@ -1,97 +1,127 @@
 import { useTranslations } from 'next-intl'
+import Link from 'next/link'
 
 export default function AboutSection() {
   const t = useTranslations('about')
 
   return (
     <>
-      <div className="h-screen px-20 py-5 flex flex-col">
-        <div className="text-8xl flex items-center gap-5">
-          <img src="/icons/home.png" alt="home-icon" className="w-[140px]" />
-          About
+      <div className="min-w-screen h-screen pl-50 py-5 flex gap-20">
+        {/* Education */}
+        <div className="w-[max-content] h-screen justify-center flex flex-col gap-[5vh] px-40 pb-[25vh]">
+          {/* title */}
+          <div className="flex items-center gap-20">
+            <span className="text-[15vw]">💭</span>
+            <span className="text-[8vw] font-semibold">Education</span>
+          </div>
+
+          {/* content */}
+          <div className="flex flex-col gap-10 pl-20">
+            <div className="text-[2.5vw] flex flex-col">
+              <div className="flex gap-10">
+                <span>❇️</span>
+                <span>️University of Birmingham | 2025-Present</span>
+              </div>
+              <span>- Master of Science, Computer Science</span>
+            </div>
+
+            <div className="text-[2.5vw] flex flex-col">
+              <div className="flex gap-10">
+                <span>❇️</span>
+                <span>Seoul National University of Education | 2018-2022</span>
+              </div>
+              <span></span>
+              <span>- BEd, Elementary Education (Mathematics)</span>
+            </div>
+          </div>
         </div>
-        {/*timeline. animation line draw. ponylink. education. think bubbles. interests (cloud).*/}
-        {/*future goal. open worldwide<br/>*/}
-        {/*put cert here. make like timeline as well. spot them and decorate with square paper-like image with interaction*/}
-        <div className="flex-1 flex items-start mt-10 ml-6 gap-10">
-          <div className="w-[45vw] flex flex-col items-center text-xl">
-            <div className="border border-black rounded-md w-[100%] h-[90%] p-2">
-              <div className="bg-green-500 text-white inline-block px-3 py-1 rounded-full font-semibold">
-                {t('experience.ponylink.title')}
-              </div>
-              <div className="p-2">
-                <div className="text-gray-500 mb-1">{t('experience.ponylink.subtitle')}</div>
-                <div className="whitespace-pre-line">{t('experience.ponylink.desc')}</div>
-              </div>
-            </div>
+
+        {/* Work */}
+        <div className="w-[max-content] h-screen justify-center flex flex-col gap-5 px-40 pb-[15vh]">
+          {/* title */}
+          <div className="flex items-center gap-20">
+            <span className="text-[15vw]">🧳</span>
+            <span className="text-[8vw] font-semibold">Work</span>
           </div>
 
-          {/*<div className="w-[30vw] flex flex-col items-center text-xl">*/}
-          {/*  <div className="border border-black rounded-md w-[100%] h-[90%] p-2">*/}
-          {/*    <div className="bg-blue-500 text-white inline-block px-3 py-1 rounded-full font-semibold">*/}
-          {/*      Certificate*/}
-          {/*    </div>*/}
-          {/*    <div className="p-2">*/}
-          {/*      <div className="whitespace-pre-line">{t('certificate.summary')}</div>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
+          {/* content */}
+          <div className="text-[2.5vw] flex flex-col gap-20">
+            <div>
+              <span>Full Stack Software Engineer @ Ponylink | Oct 2023 - Aug 2025</span>
+            </div>
 
-          <div className="w-[30vw] flex flex-col items-center text-xl">
-            <div className="border border-black rounded-md w-[100%] h-[90%] p-2">
-              <div className="bg-blue-500 text-white inline-block px-3 py-1 rounded-full font-semibold">
-                {t('certificate.aws.title')}
-              </div>
-              <div className="p-2">
-                <div className="text-gray-500 mb-1">
-                  {t('certificate.aws.institute')} | {t('certificate.aws.date')}
+            <div className="flex flex-col gap-10">
+              <div className="flex gap-5">
+                <div className="w-[350px] text-center border border-black rounded-full bg-white">
+                  FRONTEND
                 </div>
-                <div className="whitespace-pre-line">{t('certificate.aws.desc')}</div>
+                <div>Craft responsive and intuitive UIs with React & Next.js</div>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-[350px] text-center border border-black rounded-full bg-white">
+                  BACKEND
+                </div>
+                <div>Deliver reliable APIs and secure server-side solutions</div>
+              </div>
+
+              <div className="flex gap-5">
+                <div className="w-[350px] text-center border border-black rounded-full bg-white">
+                  INFRA / CLOUD
+                </div>
+                <div>Automate deployment pipelines and manage cloud operations</div>
               </div>
             </div>
+          </div>
+        </div>
+
+        {/* Certs */}
+        <div className="w-[max-content] h-screen justify-center flex flex-col gap-[5vh] px-40 pb-[15vh]">
+          {/* title */}
+          <div className="flex items-center gap-20">
+            <span className="text-[15vw]">💥</span>
+            <span className="text-[8vw] font-semibold">Certificates</span>
           </div>
 
-          <div className="w-[30vw] flex flex-col items-center text-xl">
-            <div className="border border-black rounded-md w-[100%] h-[90%] p-2">
-              <div className="bg-blue-500 text-white inline-block px-3 py-1 rounded-full font-semibold">
-                {t('certificate.sqld.title')}
-              </div>
-              <div className="p-2">
-                <div className="text-gray-500 mb-1">
-                  {t('certificate.sqld.institute')} | {t('certificate.sqld.date')}
-                </div>
-                <div className="whitespace-pre-line">{t('certificate.sqld.desc')}</div>
+          {/* content */}
+          <div className="flex flex-col gap-10 pl-20">
+            <div className="text-[2.5vw] flex flex-col">
+              <div className="flex gap-10">
+                <span>❇️</span>
+                <span>️AWS Solutions Architect Associate</span>
               </div>
             </div>
-          </div>
 
-          <div className="w-[40vw] flex flex-col items-center text-xl">
-            <div className="border border-black rounded-md w-[100%] h-[90%] p-2">
-              <div className="bg-blue-500 text-white inline-block px-3 py-1 rounded-full font-semibold">
-                {t('certificate.eip.title')}
-              </div>
-              <div className="p-2">
-                <div className="text-gray-500 mb-1">
-                  {t('certificate.eip.institute')} | {t('certificate.eip.date')}
-                </div>
-                <div className="whitespace-pre-line">{t('certificate.eip.desc')}</div>
+            <div className="text-[2.5vw] flex flex-col">
+              <div className="flex gap-10">
+                <span>❇️</span>
+                <span>️SQL Developer</span>
               </div>
             </div>
-          </div>
 
-          <div className="w-[40vw] flex flex-col items-center text-xl">
-            <div className="border border-black rounded-md w-[100%] h-[90%] p-2">
-              <div className="bg-blue-500 text-white inline-block px-3 py-1 rounded-full font-semibold">
-                {t('certificate.linux.title')}
+            <div className="text-[2.5vw] flex flex-col">
+              <div className="flex gap-10">
+                <span>❇️</span>
+                <span>Engineer of Information Processing</span>
               </div>
-              <div className="p-2">
-                <div className="text-gray-500 mb-1">
-                  {t('certificate.linux.institute')} | {t('certificate.linux.date')}
-                </div>
-                <div className="whitespace-pre-line">{t('certificate.linux.desc')}</div>
+            </div>
+
+            <div className="text-[2.5vw] flex flex-col">
+              <div className="flex gap-10">
+                <span>❇️</span>
+                <span>Linux Master (Level 2)</span>
               </div>
             </div>
           </div>
+        </div>
+
+        {/* More */}
+        <div className="w-[max-content] h-screen justify-center flex flex-col gap-[5vh] pl-20 pr-[40vw]">
+          <Link href="/about">
+            <div className="text-[3vw] border border-black px-6 rounded-full inline-block cursor-pointer bg-green-400 hover:scale-105 hover:bg-green-200">
+              ➔ Learn More About Me
+            </div>
+          </Link>
         </div>
       </div>
     </>
