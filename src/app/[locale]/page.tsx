4e08004bbcb2e029ec7ffb8d9872f1ed7b77d228
leejay <buttercookie210@gmail.com>
2025-09-30 19:27:44 +0100

    chore: render only MobileMain to test scroll issue on mobile

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,10 +6,11 @@ import MobileMain from '@/components/main/MobileHome'
 export default function HomePage() {
   return (
     <>
-      <div className="hidden md:block">
-        <DesktopMain />
-      </div>
-      <div className="block md:hidden">
+      {/*<div className="hidden md:block">*/}
+      {/*  <DesktopMain />*/}
+      {/*</div>*/}
+      {/*<div className="block md:hidden">*/}
+      <div>
         <MobileMain />
       </div>
     </>
